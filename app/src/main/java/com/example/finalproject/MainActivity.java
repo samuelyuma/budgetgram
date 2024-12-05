12a407dc242fcf860ff404766df68b681959bfd1
harvdt <harviandito20@gmail.com>
2024-12-05 15:15:23 +0700

    feat: add logo and welcome text, add icon, fix padding

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,9 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -78,8 +75,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 biometricPrompt.authenticate(promptInfo);
-                Intent intent = new Intent(MainActivity.this, CameraPageActivity.class);
-                startActivity(intent);
             }
         });
     }
